10771b01c4e43c7902fa016c33b6ab435407a5a9
kappu1999 <74296849+kappu1999@users.noreply.github.com>
2022-03-15 08:44:34 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,20 @@
+using System;
+namespace Generics1{
+    class Product{
+        public string Name ;
+
+        public string Type ;
+        public int Quantity;
+        public double Price ;
+
+        public Product(){   
+        }
+        public Product(string name,double price ,int quantity ,string type){
+            this.Name = name;
+            this.Type = type;
+            this.Quantity = quantity;
+            this.Price = price;
+        }
+
+    }
+}
